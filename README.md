ec846b127efbaa927c552434a7e6ca88f5798566
guinmoon <guinmoon@gmail.com>
2022-06-29 12:27:14 +0300

    readme update

--- README.md
+++ README.md
@@ -1,19 +1,33 @@
 Приложение запускает веб сервер который преобразует RTSP поток с камеры подключенной к роботу в  MJPEG поток и позволяет вывести его на веб страницу. Так же на странице отображается карта и элементы позволяющие управлять роботом.
+Для отображения карты необходимо чтобы на роботе была установлена прошивка [Valetudo RE](https://github.com/rand256/valetudo)
 
 # Установка
 
+**Linux:**
+```
+sudo pip3 install opencv-python
+```
+Возможно портебуется установка дополнительных пакетов
+```
+sudo apt-get install git python3-dev 
+```
+Загружаем приложение
 ```
 git clone https://github.com/guinmoon/Roborock-Project-Vacumba  
 cd Roborock-Project-Vacumba  
 pip install -r requirements.txt
 ```
 
-# Запуск
-
-```python3 vacumba_web_light.py```
-
 # Подключение камеры
 У роботов 1 поколения есть диагностический USB порт на который подается напряжение достаточное для питания камеры, на других поколениях не проверял  
 ![Roborock with IP Camera](/templates/dist/imgs/robot1.jpg)
 ![Roborock IP Camera conncet](/templates/dist/imgs/robot2.jpg)
 
+# Запуск
+
+```python3 vacumba_web_light.py```
+
+![Roborock IP Camera conncet](/templates/dist/imgs/vacumba_go.png)
+
+# Известные проблемы 
+В браузере Safari карта периодически пропадает
--- lib/bot_control.py
+++ lib/bot_control.py
@@ -7,41 +7,49 @@ from datetime import datetime
 headers = {'content-type': 'application/json'}
 
 
-def move_bot(bot_driver,ip,y_speed,x_speed,move_duration,sequenceId = 0):
-    if bot_driver=='valetudo':
-        move_bot_direct(ip,y_speed, x_speed, move_duration,sequenceId)
-    # if bot_driver=='miio':
-    #     bot.manual_control(y_speed, x_speed, move_duration)
+def move_bot(bot_driver, ip, y_speed, x_speed, move_duration, sequenceId=0, bot=None):
+    if bot_driver == 'valetudo':
+        move_bot_direct(ip, y_speed, x_speed, move_duration, sequenceId)
+    if bot_driver == 'miio':
+        bot.manual_control(y_speed, x_speed, move_duration)
 
-def move_bot_direct(ip,y_speed,x_speed,move_duration,sequenceId):
+
+def move_bot_direct(ip, y_speed, x_speed, move_duration, sequenceId):
     now = datetime.now()
-    body = '{"angle":%.4f,"velocity":%.4f,"duration":%i,"sequenceId":%i}'%(y_speed,x_speed,move_duration,sequenceId)                
-    r = requests.put('http://%s/api/set_manual_control'%ip,data= body,headers=headers)
-    #print(r)
+    body = '{"angle":%.4f,"velocity":%.4f,"duration":%i,"sequenceId":%i}' % (
+        y_speed, x_speed, move_duration, sequenceId)
+    r = requests.put('http://%s/api/set_manual_control' %
+                     ip, data=body, headers=headers)
+    # print(r)
     # self.sequenceId+=1
 
-def bot_goto(ip,x,y):
-    body = '{"x":%i,"y":%i}'%(x,y)                
-    r = requests.put('http://%s/api/go_to'%ip,data= body,headers=headers)
+
+def bot_goto(ip, x, y):
+    body = '{"x":%i,"y":%i}' % (x, y)
+    r = requests.put('http://%s/api/go_to' % ip, data=body, headers=headers)
     print(body)
     # self.sequenceId+=1
 
+
 def start_bot(ip):
     silence_bot(ip)
-    r = requests.put('http://%s/api/start_manual_control'%ip)
-    #print(r)
+    r = requests.put('http://%s/api/start_manual_control' % ip)
+    # print(r)
+
 
 def stop_bot(ip):
-    r = requests.put('http://%s/api/stop_manual_control'%ip)
-    #print(r)
+    r = requests.put('http://%s/api/stop_manual_control' % ip)
+    # print(r)
+
 
 def bot_home(ip):
-    r = requests.put('http://%s/api/drive_home'%ip)
-    #print(r)
+    r = requests.put('http://%s/api/drive_home' % ip)
+    # print(r)
+
 
 def silence_bot(ip):
     now = datetime.now()
     body = '{"speed":1}'
-    r = requests.put('http://%s/api/fanspeed'%ip,data= body,headers=headers)
+    r = requests.put('http://%s/api/fanspeed' % ip, data=body, headers=headers)
     print(r)
     # self.sequenceId+=1
